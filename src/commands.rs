f9d5484bf3aec3b649e5d43120a8a07155ee669b
Karol Zwolak <deatheyex132@gmail.com>
2022-06-04 17:29:00 +0200

    Add no-check option for encode command

--- src/commands.rs
+++ src/commands.rs
@@ -1,9 +1,9 @@
+use crate::chunk::Chunk;
+use crate::chunk_type::ChunkType;
+use clap::{Args, Parser, Subcommand};
 use std::fs::OpenOptions;
 use std::io::{Read, Write};
 use std::str::FromStr;
-use clap::{Args, Parser, Subcommand};
-use crate::chunk::Chunk;
-use crate::chunk_type::ChunkType;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -21,7 +21,7 @@ impl CliArgs {
 
 #[derive(Subcommand, Debug)]
 pub enum CommandType {
-    /// Encode a chunk with given chunk type and message into file
+    /// Encode a chunk with given chunk type and message into file (note: it checks whether file is a valid png)
     Encode(EncodeArgs),
     Decode(DecodeArgs),
 
@@ -35,7 +35,12 @@ pub struct EncodeArgs {
     file_path: String,
     chunk_type: String,
     message: String,
+    /// Outputs png file with encoded message to another file instead
+    #[clap(short, long)]
     output_file: Option<String>,
+    /// Don't validate png file provided
+    #[clap(short, long, conflicts_with = "output-file")]
+    no_check: bool,
 }
 
 #[derive(Args, Debug)]
@@ -55,8 +60,8 @@ pub struct PrintArgs {
     file_path: String,
 }
 
-use crate::Error;
 use crate::png::Png;
+use crate::Error;
 
 pub trait Command {
     fn execute_command(self) -> crate::Result<()>;
@@ -64,37 +69,44 @@ pub trait Command {
 
 impl Command for EncodeArgs {
     fn execute_command(self) -> crate::Result<()> {
-        let mut file = OpenOptions::new()
-            .write(true)
-            .read(true)
-            .create(true)
-            .open(self.file_path)?;
+        let redirect_output = self.output_file.is_none();
 
         let chunk_type = ChunkType::from_str(&self.chunk_type)?;
         let chunk_data = self.message.into_bytes();
 
         let chunk = Chunk::new(chunk_type, chunk_data);
-        let mut png_buf = Vec::new();
 
-        let len = file.read_to_end(&mut png_buf)?;
-
-        let mut png = if len == 0 {
-            Png::new()
-        } else {
-            Png::try_from(&png_buf[..])
-                .map_err(|err| Error::from(format!("Invalid png file data ({})", err)))?
-        };
-
-        png.append_chunk(chunk);
-        file.write_all(&png.as_bytes())?;
-
-        /*let mut file = OpenOptions::new() // <- todo add option to dont check png file and just append chunk
+        let mut file = OpenOptions::new()
             .write(true)
-            .append(true)
+            .read(true)
+            .create(redirect_output)
+            .append(!self.no_check)
             .open(self.file_path)?;
 
-        let bytes = chunk.as_bytes();
-        file.write_all(&bytes)?;*/
+        if !self.no_check {
+            let mut png_buf = Vec::new();
+
+            let len = file.read_to_end(&mut png_buf)?;
+
+            let mut png = if len == 0 {
+                Png::new()
+            } else {
+                Png::try_from(&png_buf[..])
+                    .map_err(|err| Error::from(format!("Invalid png file data ({})", err)))?
+            };
+
+            if redirect_output {
+                let to_file = self.output_file.unwrap();
+
+                file = OpenOptions::new().write(true).create(true).open(to_file)?;
+            }
+            file.write_all(&png.as_bytes())?;
+
+            png.append_chunk(chunk);
+        } else {
+            let bytes = chunk.as_bytes();
+            file.write_all(&bytes)?;
+        }
 
         Ok(())
     }
