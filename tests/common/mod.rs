d6ed4b092d628075d26f2d74ddf6555e206b990c
Karol Zwolak <deatheyex132@gmail.com>
2022-06-11 17:30:33 +0200

    Add decode tests and input files for these tests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -8,14 +8,22 @@ pub type TestResult = pngme::Result<()>;
 const BIN_NAME: &str = "pngme";
 
 pub const VALID_CHUNK_TYPE: &str = "ruSt";
+pub const VALID_DIFFERENT_CHUNK_TYPE: &str = "tyPe";
 pub const INVALID_CHUNK_TYPE: &str = "1234";
 
 pub const MESSAGE: &str = "top secret message";
+
+pub const SECOND_MESSAGE: &str = "second message";
+pub const DIFFERENT_MESSAGE: &str = "message with different type";
 
 pub const EMPTY_FILE: &str = "tests/inputs/empty.png";
 pub const HEADER_ONLY_FILE: &str = "tests/inputs/header_only.png";
 pub const INVALID_HEADER_FILE: &str = "tests/inputs/invalid_header.png";
 pub const VALID_FILE: &str = "tests/inputs/valid.png";
+
+pub const VALID_ENCODED1: &str = "tests/inputs/valid_encoded1.png";
+pub const VALID_ENCODED2: &str = "tests/inputs/valid_encoded2.png";
+pub const VALID_ENCODED2_DIFFERNT: &str = "tests/inputs/valid_encoded2_different.png";
 
 pub const OUTPUT_FILE: &str = "tests/test.png";
 
@@ -53,6 +61,12 @@ pub fn gen_not_existing_file() -> String {
 pub fn encode_command() -> Command {
     let mut command = command();
     command.arg("encode");
+
+    command
+}
+pub fn decode_command() -> Command {
+    let mut command = command();
+    command.arg("decode");
 
     command
 }
---
+++ tests/decode.rs
@@ -0,0 +1,80 @@
+use crate::common::{
+    decode_command, DIFFERENT_MESSAGE, EMPTY_FILE, INVALID_HEADER_FILE, MESSAGE, SECOND_MESSAGE,
+    VALID_CHUNK_TYPE, VALID_DIFFERENT_CHUNK_TYPE, VALID_ENCODED1, VALID_ENCODED2,
+    VALID_ENCODED2_DIFFERNT, VALID_FILE,
+};
+use assert_cmd::assert::Assert;
+
+mod common;
+
+fn decode(filename: &str, chunk_type: &str) -> Assert {
+    decode_command().arg(filename).arg(chunk_type).assert()
+}
+
+fn decode_message(filename: &str, chunk_type: &str, expected_message: &str) {
+    decode(filename, chunk_type)
+        .success()
+        .stdout(predicates::str::contains(format!(
+            "secret message: '{}'",
+            expected_message
+        )));
+}
+
+fn assert_no_message(filename: &str, chunk_type: &str) {
+    decode(filename, chunk_type)
+        .success()
+        .stdout(predicates::str::contains(format!(
+            "No chunk with type '{}' was found",
+            chunk_type
+        )));
+}
+
+#[test]
+fn decode_valid1() {
+    decode_message(VALID_ENCODED1, VALID_CHUNK_TYPE, MESSAGE);
+}
+
+#[test]
+fn decode_valid2_last() {
+    decode_message(VALID_ENCODED2, VALID_CHUNK_TYPE, SECOND_MESSAGE);
+    // decode_message(VALID_ENCODED2, VALID_CHUNK_TYPE, MESSAGE);
+}
+
+#[test]
+fn decode_valid2_different_last() {
+    decode_message(
+        VALID_ENCODED2_DIFFERNT,
+        VALID_DIFFERENT_CHUNK_TYPE,
+        DIFFERENT_MESSAGE,
+    );
+    // decode_message(VALID_ENCODED2_DIFFERENT, VALID_CHUNK_TYPE, MESSAGE);
+}
+
+#[test]
+fn dies_decode_empty() {
+    decode(EMPTY_FILE, VALID_CHUNK_TYPE)
+        .failure()
+        .stderr(predicates::str::contains(
+            "Invalid png file data (Valid png must have at least 8 bytes (missing header) but only 0 were provided",
+        ));
+}
+
+#[test]
+fn dies_decode_invalid() {
+    decode(INVALID_HEADER_FILE, VALID_CHUNK_TYPE)
+        .failure()
+        .stderr(predicates::str::contains(
+            "Valid png must contain valid signature header",
+        ));
+}
+
+#[test]
+fn decode_nothing_from_valid() {
+    let chunk_type = VALID_CHUNK_TYPE;
+    decode(VALID_FILE, chunk_type)
+        .success()
+        .stdout(predicates::str::contains(format!(
+            "No chunk with type '{}' was found",
+            chunk_type
+        )));
+}
